258691e0defac0e95593aacb166a0256c3e795e0
Daniel Scherzer <daniel.e.scherzer@gmail.com>
2025-04-29 12:07:39 -0700

    [Blog] Attributes on constants
    Includes integration to render code in markdown with pygments highlighting, and
    to add extra extensions on blog displays based on front matter.

--- src/Blog/BlogDisplay.php
+++ src/Blog/BlogDisplay.php
@@ -4,13 +4,27 @@ declare( strict_types = 1 );
 namespace DanielWebsite\Blog;
 
 use League\CommonMark\Environment\Environment;
+use League\CommonMark\Event\DocumentParsedEvent;
 use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\Extension\CommonMark\Node\Block\ListItem;
+use League\CommonMark\Extension\CommonMark\Node\Inline\Link;
+use League\CommonMark\Extension\CommonMark\Node\Inline\Strong;
 use League\CommonMark\Extension\ExternalLink\ExternalLinkExtension;
+use League\CommonMark\Extension\FrontMatter\FrontMatterExtension;
+use League\CommonMark\Extension\HeadingPermalink\HeadingPermalinkExtension;
 use League\CommonMark\Extension\Mention\MentionExtension;
+use League\CommonMark\Extension\TableOfContents\Node\TableOfContents;
+use League\CommonMark\Extension\TableOfContents\TableOfContentsExtension;
+use League\CommonMark\Node\Inline\Text;
+use League\CommonMark\Node\Query;
 
 class BlogDisplay {
 
-	public static function makeCommonMarkEnv( bool $realLinks ): Environment {
+	/**
+	 * Use a BlogPost object to add extra config and real links, false for
+	 * when rendering on an index/overview and not needed
+	 */
+	public static function makeCommonMarkEnv( BlogPost|false $post ): Environment {
 		$config = [
 			'mentions' => [
 				'packagist' => [
@@ -31,8 +45,15 @@ class BlogDisplay {
 				'html_class' => 'external-link',
 				'open_in_new_window' => true,
 			],
+			'heading_permalink' => [
+				'symbol' => '',
+			],
+			'table_of_contents' => [
+				'style' => 'ordered',
+				'min_heading_level' => 2,
+			],
 		];
-		if ( !$realLinks ) {
+		if ( !$post ) {
 			$config['mentions']['packagist']['generator'] = new DisabledMentionGenerator();
 			$config['mentions']['gh-repo']['generator'] = new DisabledMentionGenerator();
 		}
@@ -41,7 +62,52 @@ class BlogDisplay {
 		$env->addExtension( new CommonMarkCoreExtension() );
 		$env->addExtension( new MentionExtension() );
 		$env->addExtension( new ExternalLinkExtension() );
+		// Always gets added to the environment so that the front matter is
+		// understood and not shown
+		$frontMatterExt = new FrontMatterExtension();
+		$env->addExtension( $frontMatterExt );
+		if ( $post ) {
+			// First parse with front matter to see if extra features are needed
+			$parser = $frontMatterExt->getFrontMatterParser();
+			$parsed = $parser->parse( $post->markdown );
+			$cfg = $parsed->getFrontMatter();
+
+			$extensions = $cfg['extensions'] ?? [];
+			if ( $extensions['toc'] ?? false ) {
+				$env->addExtension( new HeadingPermalinkExtension() );
+				$env->addExtension( new TableOfContentsExtension() );
+
+				$env->addEventListener(
+					DocumentParsedEvent::class,
+					[ __CLASS__, 'onDocumentParsed' ],
+					// Lower priority than the toc generation, to run later
+					-160
+				);
+			}
+			if ( $extensions['pygments'] ?? false ) {
+				$env->addExtension( new PygmentsHighlightExtension() );
+			}
+		}
 		return $env;
+	}
+
+	public static function onDocumentParsed( DocumentParsedEvent $event ): void {
+		$doc = $event->getDocument();
+		$allTocs = ( new Query() )
+			->where( Query::type( TableOfContents::class ) )
+			->findAll( $doc );
+		foreach ( $allTocs as $toc ) {
+			$listItem = new ListItem( $toc->getListData() );
+			$link = new Link( '#', '(top)' );
+			$listItem->appendChild( $link );
+			$toc->prependChild(
+				$listItem
+			);
+
+			$strong = new Strong();
+			$strong->appendChild( new Text( "Table of contents" ) );
+			$toc->prependChild( $strong );
+		}
 	}
 
 }
