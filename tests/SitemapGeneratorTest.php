821f189edef81b405356c92964cb6a2cc2a97094
Daniel Scherzer <daniel.e.scherzer@gmail.com>
2025-09-11 12:17:20 -0700

    [Build] Convert `samdark/sitemap` to a dev dependency

--- tests/SitemapGeneratorTest.php
+++ tests/SitemapGeneratorTest.php
@@ -6,16 +6,22 @@ namespace DanielWebsite\Tests;
 /**
  * Tests that the sitemap is up to date
  */
+use DanielWebsite\Blog\BlogPostStore;
+use DanielWebsite\Router;
 use DanielWebsite\SitemapEntry;
-use DanielWebsite\SitemapGenerator;
+use FastRoute\DataGenerator\MarkBased;
+use FastRoute\RouteCollector;
+use FastRoute\RouteParser\Std;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use samdark\sitemap\Sitemap;
 
-#[CoversClass( SitemapGenerator::class )]
 #[CoversClass( SitemapEntry::class )]
 class SitemapGeneratorTest extends TestCase {
 
 	private const TEST_FILE_LOCATION = __DIR__ . '/sitemap.xml';
+	private const URL_BASE = 'https://scherzer.dev';
 
 	protected function tearDown(): void {
 		// phpcs:ignore Generic.PHP.NoSilencedErrors.Discouraged
@@ -23,8 +29,35 @@ class SitemapGeneratorTest extends TestCase {
 		parent::tearDown();
 	}
 
+	private function generateSitemap( string $location ): void {
+		$collector = new RouteCollector( new Std(), new MarkBased() );
+		Router::addRoutesCb( $collector );
+		$getPaths = $collector->getData()[0]['GET'];
+		$pages = array_unique( $getPaths );
+
+		$sitemap = new Sitemap( $location );
+		foreach ( $pages as $class ) {
+			$ref = new ReflectionClass( $class );
+			$attribs = $ref->getAttributes( SitemapEntry::class );
+			foreach ( $attribs as $attrib ) {
+				$instance = $attrib->newInstance();
+				$sitemap->addItem( self::URL_BASE . $instance->path );
+			}
+		}
+
+		$blogStore = new BlogPostStore();
+		$blogSlugs = $blogStore->listBlogSlugs();
+		// listBlogSlugs() will be in reverse order for the blog index page
+		sort( $blogSlugs );
+		foreach ( $blogSlugs as $slug ) {
+			$sitemap->addItem( self::URL_BASE . '/Blog/' . $slug );
+		}
+
+		$sitemap->write();
+	}
+
 	public function testSitemap() {
-		SitemapGenerator::generateSitemap( self::TEST_FILE_LOCATION );
+		$this->generateSitemap( self::TEST_FILE_LOCATION );
 		$expected = file_get_contents( self::TEST_FILE_LOCATION );
 
 		$actualSitemapPath = dirname( __DIR__ ) . '/sitemap.xml';
