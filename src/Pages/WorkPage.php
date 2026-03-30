240f3bd719b986ae68b0fc20e570de78331f46dd
Daniel Scherzer <daniel.e.scherzer@gmail.com>
2026-03-30 15:45:38 -0700

    [Work] Update for MUDCon 2026

--- src/Pages/WorkPage.php
+++ src/Pages/WorkPage.php
@@ -68,6 +68,20 @@ class WorkPage extends BasePage {
 			'date' => 'May 2025',
 			'video' => 'https://www.youtube.com/watch?v=-Dt-5Db1lKY',
 		],
+		[
+			'title' => 'Closing the Loop',
+			'conf' => 'MediaWiki Users and Developers Conference, Spring 2026',
+			'loc' => 'Salt Lake City, Utah',
+			'date' => 'March 2026',
+			'video' => 'https://www.youtube.com/live/_KSXOK_8yT0?si=FYool9h4f6w854Go&t=18679',
+		],
+		[
+			'title' => 'Introducing MixedVisibilityFiles',
+			'conf' => 'MediaWiki Users and Developers Conference, Spring 2026',
+			'loc' => 'Salt Lake City, Utah',
+			'date' => 'March 2026',
+			'video' => 'https://www.youtube.com/live/_KSXOK_8yT0?si=fF4kTu88hct8vcJc&t=22918',
+		],
 	];
 
 	public function __construct() {
